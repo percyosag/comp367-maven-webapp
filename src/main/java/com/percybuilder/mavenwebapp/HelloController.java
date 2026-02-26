9238e2338dedf4d400723e4b4467acfa12a2fb57
Percy Osunde <osundepo@my.centennialcollege.ca>
2026-02-26 06:41:12 -0500

    Update greeting based on time of day

--- src/main/java/com/percybuilder/mavenwebapp/HelloController.java
+++ src/main/java/com/percybuilder/mavenwebapp/HelloController.java
@@ -2,12 +2,20 @@ package com.percybuilder.mavenwebapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalTime;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to COMP367";
+
+        int hour = LocalTime.now().getHour();
+
+        if (hour < 12) {
+            return "Good morning, Percy, Welcome to COMP367";
+        } else {
+            return "Good afternoon, Percy, Welcome to COMP367";
+        }
     }
 }
